acd5db9bc051d33c16c24c493169ace931f4ca1f
Marc Collado <marc@collado.io>
2018-08-16 18:11:08 +0200

    Add file to control site colors

---
+++ src/utils/colors.js
@@ -0,0 +1,11 @@
+const black = '#2A2E2E';
+const fakeAsbestos = '#525555';
+const turquoise = '#1ABC9C';
+const eggShell = '#C8EFE8';
+
+export {
+  black,
+  fakeAsbestos,
+  turquoise,
+  eggShell,
+};
