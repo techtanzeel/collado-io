d171a08a548f613501816d4cb2864607edd9103c
Marc Collado <marc@collado.io>
2018-08-28 14:47:17 +0200

    Create component to display post tags

--- src/components/PostLink.js
+++ src/components/PostLink.js
@@ -3,47 +3,70 @@ import Link from 'gatsby-link';
 import PropTypes from 'prop-types';
 import styled from 'styled-components';
 
+import PostTags from './PostTags';
+
+import {
+  mobile, tablet,
+} from '../utils/breakpoints';
 import {
   fakeAsbestos, turquoise,
 } from '../utils/colors';
 import {
-  Title3, MetaText,
+  Title2, MetaText,
 } from '../utils/theme';
 
 const PostLink = ({ post }) => (
-  <CardLink to={post.frontmatter.path}>
-    <Container>
+  <Container>
+    <CardLink to={post.frontmatter.path}>
       <BlogPostTitle>
         {post.frontmatter.title}
       </BlogPostTitle>
-      <MetaText>
-        {`Published on ${post.frontmatter.date} →`}
-      </MetaText>
-    </Container>
-  </CardLink>
+    </CardLink>
+
+    <Excerpt>
+      {post.frontmatter.excerpt}
+    </Excerpt>
+
+    <Published>
+      {`Published on ${post.frontmatter.date}`}
+    </Published>
+
+    <PostTags
+      tags={post.frontmatter.tags}
+    />
+  </Container>
 );
+
+const Container = styled.div`
+  border-left: 0.3em solid ${fakeAsbestos}44;
+  margin-bottom: 2em;
+  padding-left: 0.8em;
+  transition: border-left 0.3s ease;
+`;
 
 const CardLink = styled(Link)`
   text-decoration: none;
 `;
 
-const BlogPostTitle = styled(Title3)`
-  margin-bottom: 0.1em;
+const BlogPostTitle = styled(Title2)`
   transition: color 0.3s ease;
-`;
-
-const Container = styled.div`
-  border-left: 0.5em solid ${fakeAsbestos}44;
-  padding-left: 0.8em;
-  transition: border-left 0.3s ease;
 
   &:hover {
-    border-left: 0.5em solid ${turquoise};
-  }
-
-  &:hover ${BlogPostTitle} {
     color: ${turquoise};
   }
+
+  &:hover ${Container} {
+    border-left: 0.3em solid ${turquoise};
+  }
+`;
+
+const Excerpt = styled(MetaText)`
+
+`;
+
+
+const Published = styled(MetaText)`
+  margin-bottom: 0.5em;
 `;
 
 PostLink.propTypes = {};
---
+++ src/components/PostTags.js
@@ -0,0 +1,57 @@
+import React from 'react';
+import Link from 'gatsby-link';
+import PropTypes from 'prop-types';
+import styled from 'styled-components';
+
+import {
+  mobile, tablet,
+} from '../utils/breakpoints';
+import {
+  fakeAsbestos, turquoise, eggShell,
+} from '../utils/colors';
+import {
+  Title3, MetaText,
+} from '../utils/theme';
+
+const renderTags = (tags) => tags.map((tag) => (
+  <TagLink
+    key={tag}
+    to={`/tags/${tag}`}
+  >
+    <TagName>
+      {`${tag}`}
+    </TagName>
+  </TagLink>
+));
+
+const PostTags = ({ tags }) => (
+  <Container>
+    {renderTags(tags)}
+  </Container>
+
+);
+
+const Container = styled.div`
+  display: flex;
+  justify-content: flex-start;
+`;
+
+const TagLink = styled(Link)`
+  text-decoration: none;
+`;
+
+const TagName = styled(MetaText)`
+  background: ${eggShell}88;
+  border-bottom: 0.2em solid ${turquoise}00;
+  border-top: 0.2em solid ${turquoise}00;
+  margin: 0em 0.5em 0.4em 0em;
+  padding: 0em 0.5em;
+
+  &:hover {
+    border-bottom: 0.2em solid ${turquoise};
+  }
+`;
+
+PostTags.propTypes = {};
+
+export default PostTags;
