cfb7331a88e0c7f03119a0c36ab370684598e22b
Marc Collado <marc@collado.io>
2018-09-12 09:21:57 +0200

    Now page sources content from md files through a GraphQL query

--- src/pages/now.js
+++ src/pages/now.js
@@ -3,17 +3,32 @@ import Link from 'gatsby-link';
 import PropTypes from 'prop-types';
 import styled from 'styled-components';
 
+import NowPost from '../components/NowPost';
 import PageHeader from '../components/PageHeader';
 import Layout from '../templates/Layout';
 
-const Now = () => {
-  console.log();
+
+const Now = ({ data }) => {
+  const Nows = data.allMarkdownRemark.edges;
+  const NowsList = Nows
+    .filter(edge => !!edge.node.frontmatter.date)
+    .map(edge => (
+      <NowPost
+        key={edge.node.id}
+        date={edge.node.frontmatter.date}
+        html={edge.node.html}
+      />
+    ));
+
   return (
     <Layout>
-      <PageHeader
-        title="Now"
-        tagline="What I'm Up To"
-      />
+      <NowPage>
+        <PageHeader
+          title="Now"
+          tagline="What I'm Up To"
+        />
+        {NowsList}
+      </NowPage>
     </Layout>
   );
 };
@@ -22,6 +37,26 @@ const NowPage = styled.div`
   margin: 0 auto;
   max-width: 800px;
   padding: 0 2em;
+`;
+
+export const nowQuery = graphql`
+  query nowQuery {
+    allMarkdownRemark(
+      filter: { fileAbsolutePath: { regex: "/(now)/" } }
+      sort: { fields: [frontmatter___date], order: DESC }
+      limit: 1000
+    ) {
+      edges {
+        node {
+          id
+          html
+          frontmatter {
+            date
+          }
+        }
+      }
+    }
+  }
 `;
 
 Now.propTypes = {};
