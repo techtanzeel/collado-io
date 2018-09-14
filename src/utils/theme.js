bc9a95882a36c9b514c7edd2a6adb3e4f7a4d3ba
Marc Collado <marc@collado.io>
2018-09-14 09:56:21 +0200

    Simplify header media queries

--- src/utils/theme.js
+++ src/utils/theme.js
@@ -15,15 +15,15 @@ const Title1 = styled.h1`
   text-transform: uppercase;
 
   @media (min-width: ${mobile}) {
-    font-size: 2.25em;
+
   }
 
   @media (min-width: ${tablet}) {
-    font-size: 2.5em;
+
   }
 
   @media (min-width: ${desktop}) {
-    font-size: 2.75em;
+
   }
 `;
 
@@ -32,19 +32,19 @@ const Title2 = styled.h2`
   font-size: 1.5em;
   font-weight: 700;
   letter-spacing: -1px;
-  margin-top: 1.5em;
+  margin-top: 1.6em;
   text-transform: uppercase;
 
   @media (min-width: ${mobile}) {
-    font-size: 1.75em;
+
   }
 
   @media (min-width: ${tablet}) {
-    font-size: 2em;
+
   }
 
   @media (min-width: ${desktop}) {
-    font-size: 2.25em;
+
   }
 `;
 
@@ -53,36 +53,36 @@ const Title3 = styled.h3`
   font-size: 1.25em;
   font-weight: 700;
   letter-spacing: -1px;
-  margin-top: 1.5em;
+  margin-top: 1.4em;
   text-transform: uppercase;
 
   @media (min-width: ${mobile}) {
-    font-size: 1.5em;
+
   }
 
   @media (min-width: ${tablet}) {
-    font-size: 1.75em;
+
   }
 
   @media (min-width: ${desktop}) {
-    font-size: 2em;
+
   }
 `;
 
 const BodyText = styled.p`
   color: ${fakeAsbestos};
-  font-size: 0.9em;
+
 
   @media (min-width: ${mobile}) {
-    font-size: 0.95em;
+
   }
 
   @media (min-width: ${tablet}) {
-    font-size: 1em;
+
   }
 
   @media (min-width: ${desktop}) {
-    font-size: 1.05em;
+
   }
 `;
 
@@ -90,7 +90,6 @@ const BodyLink = styled.a`
   border-bottom: 0.25em solid ${eggShell};
   box-shadow: inset 0 -0.25em 0 ${eggShell};
   color: inherit;
-  font-size: 0.9em;
   text-decoration: none;
   transition: background 0.3s ease;
 
@@ -108,32 +107,32 @@ const BodyLink = styled.a`
   }
 
   @media (min-width: ${mobile}) {
-    font-size: 0.95em;
+
   }
 
   @media (min-width: ${tablet}) {
-    font-size: 1em;
+
   }
 
   @media (min-width: ${desktop}) {
-    font-size: 1.05em;
+
   }
 `;
 
 const MetaText = styled.p`
   color: ${fakeAsbestos}88;
-  font-size: 0.8em;
+  font-size: 0.75em;
 
   @media (min-width: ${mobile}) {
-    font-size: 0.85em;
+    font-size: 0.8em;
   }
 
   @media (min-width: ${tablet}) {
-    font-size: 0.9em;
+    font-size: 0.85em;
   }
 
   @media (min-width: ${desktop}) {
-    font-size: 0.95em;
+    font-size: 0.9em;
   }
 `;
 
