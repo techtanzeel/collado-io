8b9d51eb0692e70dd5b76d4de1d3edd9cd07e9fe
Marc Collado <marc@collado.io>
2018-09-15 11:26:20 +0200

    Update styles and add PropTypes validation

--- src/components/TagPill.js
+++ src/components/TagPill.js
@@ -4,13 +4,13 @@ import PropTypes from 'prop-types';
 import styled from 'styled-components';
 
 import {
-  mobile, tablet,
+  mobile, tablet, desktop,
 } from '../utils/breakpoints';
 import {
-  fakeAsbestos, turquoise, eggShell,
+  turquoise, eggShell,
 } from '../utils/colors';
 import {
-  Title3, MetaText,
+  MetaText,
 } from '../utils/theme';
 
 const TagPill = ({ children }) => (
@@ -33,23 +33,32 @@ const TagLink = styled(Link)`
 
 const TagName = styled(MetaText)`
   background: ${eggShell}88;
-  border-bottom: 0.3em solid ${turquoise}00;
-  border-top: 0.3em solid ${turquoise}00;
-  margin-bottom: 0.4em; // Overrides default line-height
-  margin-right: 0.5em; // Makes some room between pills
+  border-bottom: 0.25em solid ${turquoise}00;
+  border-top: 0.25em solid ${turquoise}00;
+  margin-bottom: 0.25em; // Overrides default line-height
+  margin-right: 0.25em; // Makes some room between pills
   padding: 0em 0.5em;
 
   &:hover {
-    border-bottom: 0.3em solid ${turquoise};
+    border-bottom: 0.25em solid ${turquoise};
   }
 
-  @media (min-width: ${mobile}) {}
+  @media (min-width: ${mobile}) {
+
+  }
 
   @media (min-width: ${tablet}) {
-    padding: 0.1em 0.6em;
+    margin-right: 0.5em;
+    padding: 0.25em 0.75em;
+  }
+
+  @media (min-width: ${desktop}) {
+
   }
 `;
 
-TagPill.propTypes = {};
+TagPill.propTypes = {
+  children: PropTypes.string.isRequired,
+};
 
 export default TagPill;
