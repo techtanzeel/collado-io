2d97819a070b24474220be822ff34fb4944e0575
Marc Collado <marc@collado.io>
2018-09-22 09:17:16 +0200

    Add plugin: gatsby-remark-embed-video

--- gatsby-config.js
+++ gatsby-config.js
@@ -47,6 +47,16 @@ module.exports = {
               showCaptions: false,
             },
           },
+          {
+            resolve: 'gatsby-remark-embed-video',
+            options: {
+              width: 800,
+              ratio: 1.77,
+              height: 400,
+              related: false,
+              noIframeBorder: true,
+            },
+          },
           'gatsby-remark-copy-linked-files',
         ],
       },
