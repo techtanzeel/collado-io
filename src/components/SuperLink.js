42179196c063db1e66fd647d0f2fdd01ef82ec59
Marc Collado <marc@collado.io>
2018-10-16 08:50:52 +0200

    Create HOC that returns a Gatsby Link or <a/> depending on href props

---
+++ src/components/SuperLink.js
@@ -0,0 +1,35 @@
+import React from 'react';
+import { Link } from 'gatsby';
+import PropTypes from 'prop-types';
+import styled from 'styled-components';
+
+const SuperLink = ({
+  children, href,
+}) => (
+  href.startsWith('/')
+    ? (
+      <RelativeLink
+        to={href}
+      >
+        {children}
+      </RelativeLink>
+    )
+    : (
+      <a
+        href={href}
+      >
+        {children}
+      </a>
+    )
+);
+
+const RelativeLink = styled(Link)`
+  text-decoration: none;
+`;
+
+SuperLink.propTypes = {
+  children: PropTypes.string.isRequired,
+  href: PropTypes.string.isRequired,
+};
+
+export default SuperLink;
