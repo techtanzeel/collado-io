eb6a0f2d11e1765a927d2ceb5724a39cf40724f2
Marc Collado <marc@collado.io>
2018-11-09 23:40:13 +0100

    Closes #12 by wrapping the component with SuperLink

--- src/components/PostLink.js
+++ src/components/PostLink.js
@@ -1,11 +1,13 @@
 import React from 'react';
-import { Link } from 'gatsby';
 import PropTypes from 'prop-types';
 import styled from 'styled-components';
 
 import Card from './Card';
+import SuperLink from './SuperLink';
 
-import { turquoise } from '../utils/colors';
+import {
+  mobile, tablet, desktop,
+} from '../utils/breakpoints';
 import { Title2, BodyText, MetaText } from '../utils/theme';
 
 const PostLink = ({
@@ -14,40 +16,53 @@ const PostLink = ({
   <Card
     hasHover
   >
-    <BlogPostLink to={path}>
+    <SuperLink href={path}>
       <BlogPostTitle>
         {title}
       </BlogPostTitle>
-    </BlogPostLink>
-    <BodyText>
-      {excerpt}
-    </BodyText>
-    <Published>
-      {`Published on ${date}`}
-    </Published>
+      <Excerpt>
+        {excerpt}
+      </Excerpt>
+      <Published>
+        {`Published on ${date}`}
+      </Published>
+    </SuperLink>
   </Card>
 );
 
-const BlogPostLink = styled(Link)`
-  text-decoration: none;
+const BlogPostTitle = styled(Title2)`
+  margin: 0.5em 0em;
 `;
 
-const BlogPostTitle = styled(Title2)`
-  margin-top: 0.5em;
-  transition: color 0.3s ease;
+const Excerpt = styled(BodyText)`
+  font-size: 0.875em; // reduce 0.125em
+  margin: 0.5em 0em;
 
-  &:hover {
-    color: ${turquoise};
+  @media (min-width: ${mobile}) {}
+
+  @media (min-width: ${tablet}) {
+    font-size: 1em;
   }
+
+  @media (min-width: ${desktop}) {}
 `;
 
 const Published = styled(MetaText)`
-  margin-bottom: 0.5em;
+  font-size: 0.75em; // reduce 0.125em
+  margin: 1.5em 0em 0.5em;
+
+  @media (min-width: ${mobile}) {}
+
+  @media (min-width: ${tablet}) {
+    font-size: 0.875em;
+  }
+
+  @media (min-width: ${desktop}) {}
 `;
 
 PostLink.propTypes = {
   date: PropTypes.string.isRequired,
-  excerpt: PropTypes.string,
+  excerpt: PropTypes.string.isRequired,
   path: PropTypes.string.isRequired,
   title: PropTypes.string.isRequired,
 };
