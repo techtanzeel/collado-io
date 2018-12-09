62c4d7c321ea1e86c0a4349e1394434405bdf71e
Marc Collado <marc@collado.io>
2018-12-09 13:00:52 +0100

    Add Snackbar component

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -2,38 +2,59 @@ import React from 'react';
 import { Helmet } from 'react-helmet';
 import PropTypes from 'prop-types';
 import styled from 'styled-components';
+import { library } from '@fortawesome/fontawesome-svg-core';
+import { faTimes } from '@fortawesome/free-solid-svg-icons';
 
 import Footer from './Footer';
 import Navbar from './Navbar';
-
+import SnackBar from './SnackBar';
 import {
   mobile, tablet, desktop,
 } from '../utils/breakpoints';
-import {
-  hipBlack, fakeAsbestos, bianchiGreen,
-} from '../utils/colors';
-import {
-  Title1, Title2, Title3, BodyText, BodyLink, MetaText,
-} from '../utils/theme';
+// import {
+//   hipBlack, fakeAsbestos, bianchiGreen,
+// } from '../utils/colors';
+// import {
+//   Title1, Title2, Title3, BodyText, BodyLink, MetaText,
+// } from '../utils/theme';
 
-const Layout = ({ children }) => (
-  <Container>
-    <Helmet defaultTitle="Marc Collado">
-      <meta name="twitter:site" content="@MarcCollado" />
-      <meta name="og:type" content="website" />
-      <link
-        rel="canonical"
-        href="https://collado.io"
-      />
-      <html lang="en" />
-    </Helmet>
-    <Navbar />
-    <Content>
-      {children}
-    </Content>
-    <Footer />
-  </Container>
-);
+library.add(faTimes);
+
+class Layout extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { renderSnackBar: true };
+  }
+
+  handleUnmountSnackBar = () => {
+    this.setState({ renderSnackBar: false });
+  }
+
+  render() {
+    const { children } = this.props;
+    const { renderSnackBar } = this.state;
+
+    return (
+      <Container>
+        <Helmet defaultTitle="Marc Collado">
+          <meta name="twitter:site" content="@MarcCollado" />
+          <meta name="og:type" content="website" />
+          <link
+            rel="canonical"
+            href="https://collado.io"
+          />
+          <html lang="en" />
+        </Helmet>
+        {renderSnackBar ? <SnackBar unmount={this.handleUnmountSnackBar} /> : null}
+        <Navbar />
+        <Content>
+          {children}
+        </Content>
+        <Footer />
+      </Container>
+    );
+  }
+}
 
 const Container = styled.div`
   min-width: 320px;
