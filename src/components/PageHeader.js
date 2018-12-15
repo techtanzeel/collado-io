4e45e5c242dd7f953856b4079b621f7536fe09c5
Marc Collado <marc@collado.io>
2018-12-15 18:42:40 +0100

    No need to render images from the component itself

--- src/components/PageHeader.js
+++ src/components/PageHeader.js
@@ -2,16 +2,12 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import styled from 'styled-components';
 
-import { mobile, tablet, desktop } from '../utils/breakpoints';
 import { Title1, MetaText } from '../utils/theme';
 
-const PageHeader = ({
-  imgAlt, imgURL, tagline, title,
-}) => (
-  <Container hasImage={imgURL}>
+const PageHeader = ({ tagline, title }) => (
+  <Container>
     <Title>{title}</Title>
     <TagLine>{tagline}</TagLine>
-    {imgURL ? <Image src={imgURL} alt={imgAlt} /> : null}
   </Container>
 );
 
@@ -19,7 +15,7 @@ const Container = styled.div`
   align-items: center;
   display: flex;
   flex-direction: column;
-  margin-bottom: ${(props) => (props.hasImage ? '-2em' : '0.25em')};
+  margin-bottom: 0.25em;
 `;
 
 const Title = styled(Title1)`
@@ -31,34 +27,9 @@ const TagLine = styled(MetaText)`
   text-transform: uppercase;
 `;
 
-const Image = styled.img`
-  margin: 0em auto;
-  min-width: 12em;
-  width: 50%;
-
-  @media (min-width: ${mobile}) {
-    max-width: 16em;
-  }
-
-  @media (min-width: ${tablet}) {
-    max-width: 17em;
-  }
-
-  @media (min-width: ${desktop}) {
-    max-width: 18em;
-  }
-`;
-
 PageHeader.propTypes = {
-  imgAlt: PropTypes.string,
-  imgURL: PropTypes.string,
   tagline: PropTypes.string.isRequired,
   title: PropTypes.string.isRequired,
-};
-
-PageHeader.defaultProps = {
-  imgAlt: '',
-  imgURL: '',
 };
 
 export default PageHeader;
