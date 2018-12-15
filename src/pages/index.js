0614f56f7a52020cd209ea0646f5c11c202840da
Marc Collado <marc@collado.io>
2018-12-15 18:43:39 +0100

    Home component now handles image loading

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,43 +1,67 @@
 import React from 'react';
 import { graphql } from 'gatsby';
+import Img from 'gatsby-image';
 import PropTypes from 'prop-types';
+import styled from 'styled-components';
 
 import Layout from '../components/Layout';
 import PageHeader from '../components/PageHeader';
+import { mobile, tablet, desktop } from '../utils/breakpoints';
 import styles from '../utils/md.module.css';
 
 const Home = ({ data }) => {
-  const homeImg = data.img.childImageSharp.resolutions.src;
-  const homeText = data.text.edges[0].node.html;
+  const imageMarc = data.imageMarc.childImageSharp.fluid;
+  const copyHomePage = data.copy.edges[0].node.html;
+
   return (
     <Layout>
       <PageHeader
-        imgAlt="Marc Collado"
-        imgURL={homeImg}
         tagline="Singularly Curious"
         title="Marc Collado"
       />
+      <ImageContainer>
+        <Img
+          fluid={imageMarc}
+          alt="Marc Collado"
+          imgStyle={{ marginBottom: 0 }}
+        />
+      </ImageContainer>
       <div
         className={styles.md}
-        dangerouslySetInnerHTML={{ __html: homeText }}
+        dangerouslySetInnerHTML={{ __html: copyHomePage }}
       />
     </Layout>
   );
 };
 
+const ImageContainer = styled.div`
+  margin: 0em auto;
+  min-width: 12em;
+  width: 50%;
+
+  @media (min-width: ${mobile}) {
+    max-width: 16em;
+  }
+
+  @media (min-width: ${tablet}) {
+    max-width: 17em;
+  }
+
+  @media (min-width: ${desktop}) {
+    max-width: 18em;
+  }
+`;
+
 export const query = graphql`
   {
-    img: file(relativePath: { eq: "marc.jpg" }) {
+    imageMarc: file(relativePath: { eq: "marc.jpg" }) {
       childImageSharp {
-        sizes(maxWidth: 800) {
-          srcSet
-        }
-        resolutions {
-          src
+        fluid(maxWidth: 800) {
+          ...GatsbyImageSharpFluid
         }
       }
     }
-    text: allMarkdownRemark(
+    copy: allMarkdownRemark(
       filter: { fileAbsolutePath: { regex: "/(src)/(markdown)/(home)/" } }
     ) {
       edges {
