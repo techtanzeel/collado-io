6fd0ba766f070b2f73477094e329ad495f623635
Marc Collado <marc@collado.io>
2018-12-25 09:15:49 +0100

    Add favicon sanity

--- src/utils/seo.js
+++ src/utils/seo.js
@@ -4,9 +4,16 @@ import PropTypes from 'prop-types';
 import { StaticQuery, graphql } from 'gatsby';
 
 import Twitter from './twitter';
-import Favicon from '../../static/favicon/favicon.ico';
-import smallFavicon from '../../static/favicon/favicon-32x32.png';
-import appleTouchIcon from '../../static/favicon/apple-touch-icon.png';
+import favicon16 from '../../static/favicon/favicon-16x16.png';
+import favicon32 from '../../static/favicon/favicon-32x32.png';
+import favicon96 from '../../static/favicon/favicon-96x96.png';
+import favicon128 from '../../static/favicon/favicon-128x128.png';
+import favicon196 from '../../static/favicon/favicon-196x196.png';
+import apple120 from '../../static/favicon/apple-touch-icon-120x120.png';
+import apple152 from '../../static/favicon/apple-touch-icon-152x152.png';
+// TODO: Add 167 & 180 Apple favicons
+// import apple167 from '../../static/favicon/apple-touch-icon-167x167.png';
+// import apple180 from '../../static/favicon/apple-touch-icon-180x180.png';
 
 const query = graphql`
   query SEO {
@@ -62,10 +69,18 @@ const SEO = ({
             <meta name="apple-mobile-web-app-title" content={shortName} />
             <meta name="application-name" content={shortName} />
             <meta name="google-site-verification" content="vFwB-R5enzdQD5dGriZ1LWEt8Vs2gS9FPjXeeCg4LAI" />
-            <link rel="canonical" href="https://www.collado.io" />
-            <link rel="icon" sizes="64x64" href={Favicon} />
-            <link rel="icon" sizes="32x32" type="image/png" href={smallFavicon} />
-            <link rel="apple-touch-icon" sizes="144x144" type="image/png" href={appleTouchIcon} />
+
+            <link rel="icon" type="image/png" href={favicon16} sizes="16x16" />
+            <link rel="icon" type="image/png" href={favicon32} sizes="32x32" />
+            <link rel="icon" type="image/png" href={favicon96} sizes="96x96" />
+            <link rel="icon" type="image/png" href={favicon128} sizes="128x128" />
+            <link rel="icon" type="image/png" href={favicon196} sizes="196x196" />
+            <link rel="apple-touch-icon-precomposed" sizes="120x120" href={apple120} />
+            <link rel="apple-touch-icon-precomposed" sizes="152x152" href={apple152} />
+            {/*
+            <link rel="apple-touch-icon-precomposed" sizes="167x167" href={apple167} />
+            <link rel="apple-touch-icon-precomposed" sizes="180x180" href={apple180} />
+            */}
 
             {seo.url && <meta property="og:url" content={seo.url} />}
             {(article ? true : null) && (
