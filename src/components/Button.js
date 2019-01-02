28c3fd8732ba8f5f89782540270aaad0563923d9
Marc Collado <marc@collado.io>
2019-01-02 15:11:54 +0100

    Refactor component to align its UI with TagPill

--- src/components/Button.js
+++ src/components/Button.js
@@ -3,24 +3,19 @@ import PropTypes from 'prop-types';
 import styled from 'styled-components';
 
 import SuperLink from './SuperLink';
-
-// Size is passed via props, but the Button itself is not yet responsive
-// import { mobile, tablet, desktop } from '../utils/breakpoints';
-import { fakeAsbestos, bianchiGreen } from '../utils/colors';
+import { tablet } from '../utils/breakpoints';
+import { fakeAsbestos, hipBlack, bianchiGreen } from '../utils/colors';
 import { MetaTextMono } from '../utils/theme';
 
-const Button = ({
-  children, href, size, theme,
-}) => (
+const Button = ({ children, href, theme }) => (
   <Container>
     <SuperLink
       href={href}
     >
       <ButtonText
         theme={theme}
-        size={size}
       >
-        {children}
+        {`${children}`.toLowerCase()}
       </ButtonText>
     </SuperLink>
   </Container>
@@ -28,25 +23,35 @@ const Button = ({
 
 const Container = styled.div`
   display: flex;
+  margin-right: 0.25em;
+  margin-bottom: 0.25em;
+
+  @media (min-width: ${tablet}) {
+    margin-right: 0.5em;
+    margin-bottom: 0.5em;
+  }
 `;
 
 const ButtonText = styled(MetaTextMono)`
-  background: ${(props) => (props.theme === 'dark' ? fakeAsbestos : `${bianchiGreen}88`)};
-  border-bottom: ${(props) => (props.size === 'large' ? `0.5em` : `0.25em`)} solid transparent;
-  border-top: ${(props) => (props.size === 'large' ? `0.5em` : `0.25em`)} solid transparent;
-  color: ${(props) => (props.theme === 'dark' ? `#FFF` : `${fakeAsbestos}`)};
-  padding: ${(props) => (props.size === 'large' ? `1.25em 1.75em` : `0.5em 0.75em`)};
+  background: ${(props) => (props.theme === 'dark' ? `${fakeAsbestos}22` : `${bianchiGreen}`)};
+  border-radius: 0.25em;
+  color: ${hipBlack};
+  margin-bottom: 0em;
+  padding: 0.75em 1em;
+  transition: background 0.3s ease-out;
 
   &:hover {
-    border-bottom-width: ${(props) => (props.size === 'large' ? `0.5em` : `0.25em`)};
-    border-bottom-color: ${(props) => (props.theme === 'dark' ? `#FFF` : `${fakeAsbestos}`)};
+    background: ${(props) => (props.theme === 'dark' ? `${fakeAsbestos}44` : `${bianchiGreen}88`)};
+  }
+
+  @media (min-width: ${tablet}) {
+    padding: 1em 1.25em;
   }
 `;
 
 Button.propTypes = {
   children: PropTypes.string.isRequired,
   href: PropTypes.string.isRequired,
-  size: PropTypes.oneOf(['small', 'large']).isRequired,
   theme: PropTypes.oneOf(['light', 'dark']).isRequired,
 };
 
