81cc3f43d29c81ed500ed3d400f0cdd65d9a022e
Marc Collado <marc@collado.io>
2019-01-24 20:34:31 +0100

    Format arrays inline

--- src/utils/typography.js
+++ src/utils/typography.js
@@ -4,32 +4,18 @@ const typography = new Typography({
   googleFonts: [
     {
       name: 'IBM Plex Mono',
-      styles: [
-        '400',
-        '700',
-      ],
+      styles: ['400', '700']
     },
     {
       name: 'IBM Plex Sans',
-      styles: [
-        '400',
-        '400i',
-        '700',
-        '700i',
-      ],
-    },
+      styles: ['400', '400i', '700', '700i']
+    }
   ],
   baseFontSize: '16px',
   baseLineHeight: 1.65,
   scaleRatio: 1.75,
-  headerFontFamily: [
-    'IBM Plex Mono',
-    'monospace',
-  ],
-  bodyFontFamily: [
-    'IBM Plex Sans',
-    'sans-serif',
-  ],
+  headerFontFamily: ['IBM Plex Mono', 'monospace'],
+  bodyFontFamily: ['IBM Plex Sans', 'sans-serif']
 });
 
 module.exports = typography;
