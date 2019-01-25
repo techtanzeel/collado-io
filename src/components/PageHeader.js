0cd4ae819cc6ebc8844b577d738c807edab91709
Marc Collado <marc@collado.io>
2019-01-25 17:15:29 +0100

    Remove styled components in favor of local styles

--- src/components/PageHeader.js
+++ src/components/PageHeader.js
@@ -1,37 +1,18 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import styled from 'styled-components';
-
-import { Title1, MetaText } from '../utils/theme';
 
 const PageHeader = ({ tagline, title }) => (
-  <Container>
-    <Title>{title}</Title>
-    <TagLine>{tagline}</TagLine>
-  </Container>
+  <div className="flex header">
+    <h1 style={{ margin: 0, padding: '0em 0.5em' }}>{title}</h1>
+    <p className="meta tagline" style={{ margin: '0.25em 0' }}>
+      {tagline}
+    </p>
+  </div>
 );
-
-const Container = styled.div`
-  align-items: center;
-  display: flex;
-  flex-direction: column;
-  margin-bottom: 0.25em;
-`;
-
-const Title = styled(Title1)`
-  margin: 0em auto 0.25em;
-`;
-
-const TagLine = styled(MetaText)`
-  line-height: 1.25em;
-  margin-bottom: 1em;
-  text-align: center;
-  text-transform: uppercase;
-`;
 
 PageHeader.propTypes = {
   tagline: PropTypes.string.isRequired,
-  title: PropTypes.string.isRequired,
+  title: PropTypes.string.isRequired
 };
 
 export default PageHeader;
