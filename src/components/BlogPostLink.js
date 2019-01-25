c7edf0cc73a0803cf4ce0ae335bb82ce1fcf421e
Marc Collado <marc@collado.io>
2019-01-25 16:40:55 +0100

    Remove styled components in favor of local styles

---
+++ src/components/BlogPostLink.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import Card from './Card';
+
+const BlogPostLink = ({ date, excerpt, path, title }) => (
+  <Card hover path={path}>
+    <h2>{title}</h2>
+    <p className="excerpt">{excerpt}</p>
+    <p className="meta">{`Published on ${date}`}</p>
+  </Card>
+);
+
+BlogPostLink.propTypes = {
+  date: PropTypes.string.isRequired,
+  excerpt: PropTypes.string,
+  path: PropTypes.string.isRequired,
+  title: PropTypes.string.isRequired
+};
+
+BlogPostLink.defaultProps = {
+  excerpt: ''
+};
+
+export default BlogPostLink;
