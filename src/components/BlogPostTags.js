c325db59c06063dd923723aa611bd5d01d50e23a
Marc Collado <marc@collado.io>
2019-01-25 16:58:10 +0100

    Remove styled components in favor of local styles

--- src/components/BlogPostTags.js
+++ src/components/BlogPostTags.js
@@ -1,29 +1,17 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import styled from 'styled-components';
-
 import TagPill from './TagPill';
 
 const BlogPostTags = ({ tags }) => (
-  <Container>
+  <div className="flex tags">
     {tags.map((tag) => (
-      <TagPill
-        key={tag}
-      >
-        {tag}
-      </TagPill>
+      <TagPill key={tag}>{tag}</TagPill>
     ))}
-  </Container>
+  </div>
 );
 
-const Container = styled.div`
-  display: flex;
-  flex-wrap: wrap;
-  justify-content: center;
-`;
-
 BlogPostTags.propTypes = {
-  tags: PropTypes.arrayOf(PropTypes.string).isRequired,
+  tags: PropTypes.arrayOf(PropTypes.string).isRequired
 };
 
 export default BlogPostTags;
