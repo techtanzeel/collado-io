64d2063d76df2ae4e96d155ba7da995473ec9793
Marc Collado <marc@collado.io>
2019-01-26 08:29:33 +0100

    Reset margins

--- src/components/BlogPostLink.js
+++ src/components/BlogPostLink.js
@@ -4,7 +4,7 @@ import Card from './Card';
 
 const BlogPostLink = ({ date, excerpt, path, title }) => (
   <Card hover path={path}>
-    <h2>{title}</h2>
+    <h2 style={{ margin: 0 }}>{title}</h2>
     <p className="excerpt">{excerpt}</p>
     <p className="meta">{`Published on ${date}`}</p>
   </Card>
