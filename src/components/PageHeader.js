50d59dfbe3a8824e52634dfac12f5a5b9fea1ed4
Marc Collado <marc@collado.io>
2019-01-27 16:42:24 +0100

    Remove em from padding

--- src/components/PageHeader.js
+++ src/components/PageHeader.js
@@ -3,7 +3,7 @@ import PropTypes from 'prop-types';
 
 const PageHeader = ({ tagline, title }) => (
   <div className="flex header">
-    <h1 style={{ margin: 0, padding: '0em 0.5em' }}>{title}</h1>
+    <h1 style={{ margin: 0, padding: '0 0.5em' }}>{title}</h1>
     <p className="meta tagline" style={{ margin: '0.25em 0' }}>
       {tagline}
     </p>
