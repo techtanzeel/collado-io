b29946dda141358e9f94e8e3d564c3b2f69879a2
Marc Collado <marc@collado.io>
2019-03-05 14:18:49 +0100

    Refactor Navbar component to have its own styles

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { Link } from 'gatsby';
-import styles from '../utils/nav.module.css';
+import styles from './Navbar.module.css';
 
 const navItems = ['home', 'blog', 'work', 'now'];
 
@@ -8,8 +8,8 @@ const Navbar = () => (
   <div className={styles.navbar}>
     {navItems.map((item) => (
       <Link
-        className={styles.navlink}
-        style={{ color: `#FFF` }}
+        className={styles.navbar__link}
+        style={{ color: 'white' }}
         activeStyle={{ borderBottom: `0.25em solid #FFF4` }}
         key={item}
         to={item === `home` ? `/` : `/${item}`}
