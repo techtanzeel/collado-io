20f765de7c4d858d0f52e8e888acc0f31c976b82
Marc Collado <marc@collado.io>
2019-03-07 17:57:08 +0100

    Update placeholder content

--- src/pages/work/pansa.js
+++ src/pages/work/pansa.js
@@ -1,33 +1,26 @@
 import React from 'react';
-import { graphql } from 'gatsby';
 import Img from 'gatsby-image';
-import PropTypes from 'prop-types';
-import styled from 'styled-components';
-import { Tabs, TabList, Tab, TabPanel } from 'react-tabs';
-
-import { BlogCard } from '../../components/BlogCard';
 import Layout from '../../components/Layout';
 import PageHeader from '../../components/PageHeader';
 import '../../utils/tabs.css';
-import { BodyText } from '../../utils/theme';
 
 const pansa = () => (
   <Layout>
     <PageHeader tagline="Cloud storage for your mind " title="pansa" />
-    <BodyText>
+    <p>
       Pansa is a memory-taking app. It helps you create, organize, find and
       connect memories so you don’t have to worry about forgetting. Save
       anything you want to remember in Pansa and next time you want to access
       this information you’ll be able to easily find it.
-    </BodyText>
-    <BodyText>
+    </p>
+    <p>
       This side-project — and this page — is currently under development and it
       will be released some time in 2019.
-    </BodyText>
-    <BodyText>
+    </p>
+    <p>
       Meanwhile you can find out more in the{' '}
       <a href="https://github.com/MarcCollado/pansa">project repository</a>.
-    </BodyText>
+    </p>
   </Layout>
 );
 
