a87375ddf182acb62411fd96d13f7018ea40612a
Marc Collado <marc@collado.io>
2019-03-07 07:12:36 +0100

    Optimize component to align with global styles refactor

--- src/components/WorkCard/WorkCard.js
+++ src/components/WorkCard/WorkCard.js
@@ -2,23 +2,19 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import { Link, StaticQuery, graphql } from 'gatsby';
 import Img from 'gatsby-image';
-import styled from 'styled-components';
-import { mobile, tablet, desktop } from '../../utils/breakpoints';
-
 import styles from './WorkCard.module.css';
 
-// Related component: Card.js
-const WorkCard = ({ children, data, excerpt, path, title }) => (
+const WorkCard = ({ data, excerpt, path, title }) => (
   <div className={styles.card}>
     <Link className={styles.card__link} to={path}>
       <Img
         fluid={data[title.toLowerCase()].childImageSharp.fluid}
         alt={title}
       />
-      <TextContainer>
+      <div className={styles.card__wrapper}>
         <h2 className={styles.card__title}>{title}</h2>
         <p className={styles.card__excerpt}>{excerpt}</p>
-      </TextContainer>
+      </div>
     </Link>
   </div>
 );
@@ -27,6 +23,13 @@ export default (props) => (
   <StaticQuery
     query={graphql`
       query WorkCardQuery {
+        pansa: file(relativePath: { eq: "pansa.png" }) {
+          childImageSharp {
+            fluid(maxWidth: 500) {
+              ...GatsbyImageSharpFluid
+            }
+          }
+        }
         ironhack: file(relativePath: { eq: "ironhack.png" }) {
           childImageSharp {
             fluid(maxWidth: 500) {
@@ -47,26 +50,7 @@ export default (props) => (
   />
 );
 
-// Unlike Card.js padding is set within the TextContainer
-// because of the top image that takes the full width
-const TextContainer = styled.div`
-  padding: 0.75em 1.25em;
-
-  @media (min-width: ${mobile}) {
-    padding: 1em 1.5em;
-  }
-
-  @media (min-width: ${tablet}) {
-    padding: 1.25em 1.75em;
-  }
-
-  @media (min-width: ${desktop}) {
-    padding: 1.25em 2em;
-  }
-`;
-
 WorkCard.propTypes = {
-  children: PropTypes.arrayOf(PropTypes.object).isRequired,
   data: PropTypes.shape({
     iomando: PropTypes.object,
     ironhack: PropTypes.object
