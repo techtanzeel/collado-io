b07023b8bec8eefff2f21e20c36a22b6597a7413
Marc Collado <marc@collado.io>
2019-03-07 13:20:35 +0100

    Add PublishedAt module

--- src/components/NowCard/NowCard.js
+++ src/components/NowCard/NowCard.js
@@ -1,12 +1,13 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import styles from './NowCard.module.css';
+import { PublishedAt } from '../PublishedAt';
 
 const NowCard = ({ title, html, date }) => (
   <div className={styles.card}>
     <h2>{title}</h2>
     <div dangerouslySetInnerHTML={{ __html: html }} />
-    <p>{`Updated on ${date}`}</p>
+    <PublishedAt date={date} />
   </div>
 );
 
