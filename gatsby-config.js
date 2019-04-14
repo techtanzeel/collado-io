d9593b3728882c383d0ca31acdb6313ecd9ca99a
Marc Collado <marc@collado.io>
2019-04-14 09:11:31 +0200

    Add plugin: gatsby-remark-autolink-headers

--- gatsby-config.js
+++ gatsby-config.js
@@ -54,6 +54,7 @@ module.exports = {
       resolve: 'gatsby-transformer-remark',
       options: {
         plugins: [
+          'gatsby-remark-autolink-headers',
           'gatsby-remark-copy-linked-files',
           {
             resolve: 'gatsby-remark-images',
